83f50d5b353962a3353ae959d3eec5ce7c93e5da
DFC <69485115+DamieFC@users.noreply.github.com>
2020-11-23 14:20:11 -0800

    Create soup is good food.py

---
+++ commands/soup is good food.py
@@ -0,0 +1,24 @@
+userInput = input("you@platypusos:~# ")
+if userInput = "office":
+	print('''OFFICE!? I HATE OFFICE!! DON'T YOU KNOW THE DEAD KENNEDYS!?
+	"Soup is good food --we don't need you anymore
+	You made a good meal --we don't need you anymore
+	So how does it feel --we don't need you anymore
+	To be like **** out our ***
+	And thrown in the cold like a peice of trash
+	
+	--neat guitar/bass solo thing--
+	
+	Were sorry
+	we hate to inturrupt
+	but it's against the law to jump off this bridge
+	You'll just have to kill yourself somewhere else
+	a tourist might see you and we wouldn't want that"
+	
+	
+	Jello Biafra also said something about computers don't go on strike, and that unemployment ran out in just 3 	weeks. Sooooo...
+	Basically, he's saying offices and   B  I  G   corporations eat you up and spit you out. So if you are thinking about working in an office--DON'T! YOU'LL  D I E  IN THERE!!!''')
+	userInput
+	else:
+		print("Wut?")
+		userInput
