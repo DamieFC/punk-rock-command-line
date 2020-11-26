3f567bd7331dae666d2c114b11c9f438ad036655
DFC <69485115+DamieFC@users.noreply.github.com>
2020-11-26 15:08:46 -0800

    Add files via upload

---
+++ commands/got no right to know.py
@@ -0,0 +1,8 @@
+userInput = input("you@platypusos:~# ")
+if userInput = "What do I need to know":
+	print('''What you need to know:
+	"got no right to know."
+	period
+	"Got no right to know" from Funeral Oration's "Got no right to know", 1980 something. Honestly not sure.''')
+else: print('''What?
+'''userInput)
